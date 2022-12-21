fd187b770efe74d8f6bbfbd151e3fc6327c49d5f
Nisha Subbarayagowda <Nisha.Subbarayagowda@mab.org.uk>
2022-12-21 15:02:38 +0000

    button working

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => (
             <SubHeading title="Chase the new flavour" />
             <h1 className="app__header-h1">The Key to Fine Dinig</h1>
             <p className="p__opensans" style={{ margin: '2rem 0' }}>The New Dining Amazing Just Try It!</p>
-            <button type="button" className="custom__button">Explore Menu</button>
+            <a href="#menu"><button type="button" className="custom__button" href>Explore Menu</button></a>
         </div>
         <div className="app__wrapper_img">
             <img src={images.welcome} alt="header-img" />
